1de8230539d6b334f74a37fb8087d9f2bdb283b0
Leo Riesenbach <leoriesenbach@gmail.com>
2026-01-18 18:43:13 -0500

    Move print eval to interpret_expr instead of interpret_statement

--- tests/common/interpreter.rs
+++ tests/common/interpreter.rs
@@ -2,14 +2,19 @@ use crate::common::ValueEnv;
 use cs4999_compiler::ast::*;
 use std::collections::VecDeque;
 
-fn interpret_expr(e: &Expr, inputs: &mut VecDeque<i64>, env: &mut ValueEnv) -> Option<i64> {
+fn interpret_expr(
+    e: &Expr,
+    inputs: &mut VecDeque<i64>,
+    outputs: &mut VecDeque<i64>,
+    env: &mut ValueEnv,
+) -> Option<i64> {
     use Expr::*;
 
     match e {
         BinaryOp(left, op, right) => {
-            let l_val = interpret_expr(&*left, inputs, env)
+            let l_val = interpret_expr(&*left, inputs, outputs, env)
                 .expect(format!("{:?} didn't evaluate to a constant", *left).as_str());
-            let r_val = interpret_expr(&*right, inputs, env)
+            let r_val = interpret_expr(&*right, inputs, outputs, env)
                 .expect(format!("{:?} didn't evaluate to a constant", *right).as_str());
             match op {
                 BinaryOperator::Add => Some(l_val + r_val),
@@ -17,13 +22,15 @@ fn interpret_expr(e: &Expr, inputs: &mut VecDeque<i64>, env: &mut ValueEnv) -> O
                 BinaryOperator::Multiply => Some(l_val * r_val),
                 BinaryOperator::Divide => Some(l_val / r_val),
             }
-        },
-        UnaryOp(UnaryOperator::Minus, v) => {
-            Some(-(interpret_expr(&*v, inputs, env).expect(format!("{v:?} didn't evalute to a constant").as_str())))
-        },
-        UnaryOp(UnaryOperator::Plus, v) => {
-            Some(interpret_expr(&*v, inputs, env).expect(format!("{v:?} didn't evalute to a constant").as_str()))
-        },
+        }
+        UnaryOp(UnaryOperator::Minus, v) => Some(
+            -(interpret_expr(&*v, inputs, outputs, env)
+                .expect(format!("{v:?} didn't evalute to a constant").as_str())),
+        ),
+        UnaryOp(UnaryOperator::Plus, v) => Some(
+            interpret_expr(&*v, inputs, outputs, env)
+                .expect(format!("{v:?} didn't evalute to a constant").as_str()),
+        ),
         Constant(Value::I64(v)) => Some(*v),
         Call(name, args) => {
             if name == &Identifier::Named(String::from("input_int")) && args.is_empty() {
@@ -49,28 +56,38 @@ fn interpret_expr(e: &Expr, inputs: &mut VecDeque<i64>, env: &mut ValueEnv) -> O
     }
 }
 
-fn interpret_statement(s: &Statement, inputs: &mut VecDeque<i64>, outputs: &mut VecDeque<i64>, remaining_stmts: &[Statement], env: &mut ValueEnv) {
+fn interpret_statement(
+    s: &Statement,
+    inputs: &mut VecDeque<i64>,
+    outputs: &mut VecDeque<i64>,
+    remaining_stmts: &[Statement],
+    env: &mut ValueEnv,
+) {
     match s {
-        Statement::Expr(Expr::Call(name, args)) => {
-            if name == "print" && args.len() == 1 {
-                let val = interpret_expr(&args[0], inputs, env).expect(format!("{:?} didn't evaluate to a constant", args[0]).as_str());
-                outputs.push_back(val);
-            } else {
-                unimplemented!("Invalid call statement")
-            }
-            if !remaining_stmts.is_empty() {
-                interpret_statement(&remaining_stmts[0], inputs, outputs, &remaining_stmts[1..], env)
-            }
-        },
         Statement::Expr(e) => {
-            interpret_expr(e, inputs, env);
+            interpret_expr(e, inputs, outputs, env);
             if !remaining_stmts.is_empty() {
-                interpret_statement(&remaining_stmts[0], inputs, outputs, &remaining_stmts[1..], env)
+                interpret_statement(
+                    &remaining_stmts[0],
+                    inputs,
+                    outputs,
+                    &remaining_stmts[1..],
+                    env,
+                )
             }
-        },
-        _ => {
+        }
+        Statement::Assign(id, e) => {
+            let result = interpret_expr(e, inputs, outputs, env);
+            env.insert(id.clone(), Value::I64(result.expect(format!("Expr `{e:?} did not return a value").as_str())));
+
             if !remaining_stmts.is_empty() {
-                interpret_statement(&remaining_stmts[0], inputs, outputs, &remaining_stmts[1..], env)
+                interpret_statement(
+                    &remaining_stmts[0],
+                    inputs,
+                    outputs,
+                    &remaining_stmts[1..],
+                    env,
+                )
             }
         }
     };
