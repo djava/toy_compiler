7937b93026033c81c3912968d0afc02afb051061
Leo Riesenbach <leoriesenbach@gmail.com>
2026-03-10 13:48:37 -0400

    Add test to make sure output assembles in integration tests

--- crates/compiler/tests/utils.rs
+++ crates/compiler/tests/utils.rs
@@ -1,5 +1,8 @@
 pub use std::collections::VecDeque;
+use std::io::Write;
+use tempfile::NamedTempFile;
 
+use compiler::syntax_trees::x86::X86Program;
 use test_support::{
     compiler::{pipeline::Pipeline, syntax_trees::parser},
     x86_interpreter::interpret_x86,
@@ -28,6 +31,10 @@ pub fn execute_test_case(mut tc: TestCase) {
     let mut no_opt_outputs = VecDeque::<i64>::new();
     interpret_x86(&no_opt_x86, &mut no_opt_tc.inputs, &mut no_opt_outputs);
     assert_eq!(no_opt_outputs, no_opt_tc.expected_outputs);
+    let no_opt_assembler_res = run_assembler(no_opt_x86);
+    if let Err(e) = no_opt_assembler_res {
+        panic!("Failed assembler: {e}")
+    }
 
     let opt_pipeline = Pipeline::make_opt();
     let opt_x86 = opt_pipeline.run(ast);
@@ -36,4 +43,45 @@ pub fn execute_test_case(mut tc: TestCase) {
     let mut opt_outputs = VecDeque::<i64>::new();
     interpret_x86(&opt_x86, &mut tc.inputs, &mut opt_outputs);
     assert_eq!(opt_outputs, tc.expected_outputs, "Failed in optimized only");
+    let opt_assembler_res = run_assembler(opt_x86);
+    if let Err(e) = opt_assembler_res {
+        panic!("Failed assembler in optimized only: {e}")
+    }
+}
+
+fn run_assembler(p: X86Program) -> Result<(), String> {
+    let asm = format!("{p}");
+
+    let out_file = NamedTempFile::new().map_err(|e| format!("failed to create temp file: {e}"))?;
+    let mut asm_command = std::process::Command::new("gcc")
+        .args([
+            "-x",
+            "assembler",
+            "-c",
+            "-o",
+            out_file.path().to_str().unwrap(),
+            "-",
+        ])
+        .stdin(std::process::Stdio::piped())
+        .stdout(std::process::Stdio::piped())
+        .stderr(std::process::Stdio::piped())
+        .spawn()
+        .map_err(|e| format!("failed to spawn gcc: {e}"))?;
+
+    asm_command
+        .stdin
+        .take()
+        .unwrap()
+        .write_all(asm.as_bytes())
+        .map_err(|e| format!("Failed to write to stdin: {e}"))?;
+
+    let output = asm_command
+        .wait_with_output()
+        .map_err(|e| format!("Failed to wait for gcc: {e}"))?;
+
+    if output.status.success() {
+        Ok(())
+    } else {
+        Err(String::from_utf8_lossy(&output.stderr).into_owned())
+    }
 }
